1eeadd2f37f4b98977d9785dbb3d0d5fcec75d3e
Julien Urraca <julien@floxdev.com>
2023-01-30 15:36:19 -0600

    flox setup

--- pkgs/hello-python/default.nix
+++ pkgs/hello-python/default.nix
@@ -1,7 +1,11 @@
-{self, python3Packages}:
+{
+  self,
+  python3Packages,
+  lib,
+}:
 python3Packages.buildPythonPackage {
   pname = "hello-python";
-  version = "0.0.0";
+  version = "0.0.0-${lib.flox-floxpkgs.getRev self}";
   src = self; # + "/src";
   PIP_DISABLE_PIP_VERSION_CHECK = 1;
   # Add Python modules needed by your package here
